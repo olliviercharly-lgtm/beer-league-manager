096c8d563e37edc7d4694544ce7769c73c908ebb
olliviercharly-lgtm <ollivier.charly@gmail.com>
2026-09-25 17:15:47 +0000

    Fix: runtime edge pour le middleware

--- middleware.ts
+++ middleware.ts
@@ -29,6 +29,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
+  runtime: 'edge',
   matcher: [
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
