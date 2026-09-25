ced32a8448f3737be1945bc50afb9fe7e289023e
olliviercharly-lgtm <ollivier.charly@gmail.com>
2026-09-25 17:22:55 +0000

    Revert: runtime middleware par defaut

--- middleware.ts
+++ middleware.ts
@@ -29,7 +29,6 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  runtime: 'experimental-edge',
   matcher: [
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
